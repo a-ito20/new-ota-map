4feb7c47b337af7202c64b1e9a6324d3548709ff
a-ito20 <110584402+a-ito20@users.noreply.github.com>
2022-08-04 14:32:07 +0900

    Update constants.js

--- constants.js
+++ constants.js
@@ -1,2 +1,2 @@
-const API_KEY = 'AIzaSyDnfbPmSRNry-6S9MkIyM5gzGbtZSKj2d4';
+const API_KEY = 'AIzaSyDTTTpdkp4PsAa4mawFgaDtc9sXi84JKJo';
 const SHEET_ID = '1bg10JXNUSOiFK47XEFlivugVOuXbQdYooAEJz62oX6A';
